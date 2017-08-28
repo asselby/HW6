e3b0dc97fc2c2f1ade213b275d1b37b0f636a763
БаймуханбетоваА <@208-08-PC>
2017-08-28 15:23:41 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ArrayIndexer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ArrayIndexer
+    {
+        int[] array = new int[100];
+        public int this[int i]
+        {
+            get { return array[i]; }
+            set { array[i] = value * value; }
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayIndexer array = new ArrayIndexer();
+            array[2] = 10;
+            Console.WriteLine("array[2]-{0}", array[2]);
+            Console.ReadLine();
+        }
+    }
+}
